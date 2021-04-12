537ed4a86b6bd64f4b7085abecce13fe111283d4
Michael Rose <michael_rose@gmx.de>
2021-04-12 12:37:17 +0200

    setup management UI

--- src/components/realm-app-manager/app-manager.tsx
+++ src/components/realm-app-manager/app-manager.tsx
@@ -1,22 +1,44 @@
 import Button from '@leafygreen-ui/button';
+import Badge from '@leafygreen-ui/badge';
+import { RealmLogoMark } from '@leafygreen-ui/logo';
+import { Body, H2 } from '@leafygreen-ui/typography';
 import { Redirect } from 'react-router';
 import { useRealm } from '../../context/realm';
+import { Spacer } from '../../typography';
+import './app-manager.less';
 
 export const RealmAppManager: React.FC = () => {
   const realm = useRealm();
   if (!realm.realmServiceData) {
     return <Redirect to="/configure" />;
   }
+  if (!realm.serviceApi) {
+    realm.clear();
+    return <Redirect to="/configure" />;
+  }
 
   return (
-    <>
-      <h1>Realm</h1>
+    <div className="realm-app-manager">
+      <header className="realm-app-manager__header">
+        <RealmLogoMark size={64} />
+        <Spacer direction="horizontal" />
 
-      <Button
-        onClick={() => realm.clear()}
-      >
-        Clear Realm Data
-      </Button>
-    </>
+        <div className="realm-app-manager__title">
+          <H2>Rapid Realm</H2>
+          <Body>
+            Managing App <Badge>{realm.serviceApi.getApp().getDetails().name}</Badge> &mdash; Service <Badge>{realm.serviceApi.getDetails().name}</Badge>
+          </Body>
+        </div>
+
+        <Spacer direction="flex" />
+
+        <Button
+          onClick={() => realm.clear()}
+        >
+          Logout
+        </Button>
+      </header>
+
+    </div>
   );
 }
